7a72a338b002a7037c98a8f037c34f19a4fd39b0
NunoSempere <nuno.sempere@protonmail.com>
2023-04-06 16:41:20 -0400

    feat: make display much nicer

--- components/displayers/dynamicSquiggleChart.js
+++ components/displayers/dynamicSquiggleChart.js
@@ -26,6 +26,7 @@ export function DynamicSquiggleChart({ squiggleChartCode }) {
       <div className="grid place-items-center">
         <div className="grid place-items-center bg-white w-11/12 p-8">
           <div className=" max-w-2xl">
+						<div className="squiggle">
             <SquiggleChart code={squiggleChartCode}
               width={672}
               height={1000}
@@ -33,6 +34,7 @@ export function DynamicSquiggleChart({ squiggleChartCode }) {
               distributionChartSettings={
                 {showSummary: true}
               }/>
+						</div>
           </div>
         </div>
       </div>
