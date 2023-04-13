f59cb5403cb575939dc7e0785ed6e546a115c7af
NunoSempere <nuno.sempere@protonmail.com>
2023-04-13 16:30:44 -0400

    tweak: commit meant to be reverted
    Just trying some things to fix import error

--- components/displayers/dynamicNativeSquiggleEditor.js
+++ components/displayers/dynamicNativeSquiggleEditor.js
@@ -1,7 +1,11 @@
 import React, {useRef, useState, useEffect} from "react";
 import dynamic from "next/dynamic";
 
-const SquiggleEditor = dynamic(() => import ("@quri/squiggle-components").then((mod) => mod.SquiggleEditor), {
+const SquiggleEditor = dynamic(async () => { 
+    const {SquiggleEditor} = await import ("@quri/squiggle-components/dist/esm/src/index.js")
+
+    return SquiggleEditor
+  }, {
   loading: () => <p>Loading...</p>,
   ssr: false
 });
